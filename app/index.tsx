f692e33053b486a309cc66caec446975be839223
Fabricio <fabricio@leadboxhq.com>
2025-01-01 17:18:08 -0400

    button styles and schema part 2

--- app/index.tsx
+++ app/index.tsx
@@ -22,8 +22,40 @@ const CalculatorApp = () => {
           <CalculatorButton label='C' blackText color={Colors.lightGray} onPress={() => console.log('C')} />
           <CalculatorButton label='+/-' blackText color={Colors.lightGray} onPress={() => console.log('+/-')} />
           <CalculatorButton label='del' blackText color={Colors.lightGray} onPress={() => console.log('del')} />
-          <CalculatorButton label='÷'  onPress={() => console.log('÷')} />
+          <CalculatorButton label='÷'  onPress={() => console.log('÷')} color={Colors.orange} />
         </View>
+
+        {/* Fila de botones */}
+        <View style={globalStyles.row}>
+          <CalculatorButton label='7'  onPress={() => console.log('7')} />
+          <CalculatorButton label='8'  onPress={() => console.log('8')} />
+          <CalculatorButton label='9'  onPress={() => console.log('9')} />
+          <CalculatorButton label='X'  onPress={() => console.log('X')} color={Colors.orange} />
+        </View>
+
+        {/* Fila de botones */}
+        <View style={globalStyles.row}>
+          <CalculatorButton label='4' onPress={() => console.log('4')} />
+          <CalculatorButton label='5' onPress={() => console.log('5')} />
+          <CalculatorButton label='6' onPress={() => console.log('6')} />
+          <CalculatorButton label='-'  onPress={() => console.log('-')} color={Colors.orange} />
+        </View>
+
+        {/* Fila de botones */}
+        <View style={globalStyles.row}>
+          <CalculatorButton label='1' onPress={() => console.log('1')} />
+          <CalculatorButton label='2' onPress={() => console.log('2')} />
+          <CalculatorButton label='3' onPress={() => console.log('3')} />
+          <CalculatorButton label='+' onPress={() => console.log('+')} color={Colors.orange} />
+        </View>
+
+        {/* Fila de botones */}
+        <View style={globalStyles.row}>
+          <CalculatorButton label='0' onPress={() => console.log('0')} doubleSize={true} />
+          <CalculatorButton label='.' onPress={() => console.log('.')} />
+          <CalculatorButton label='='  onPress={() => console.log('=')} color={Colors.orange} />
+        </View>
+
       </View>
     </View>
   )
--- components/CalculatorButton.tsx
+++ components/CalculatorButton.tsx
@@ -1,19 +1,26 @@
 import { View, Text, Pressable } from 'react-native'
 import React from 'react'
 import { globalStyles } from '@/styles/global-styles'
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { Colors } from '@/constants/Colors';
+
 
 interface Props {
   label: string;
   color?: string;
+  doubleSize?: boolean;
   blackText?: boolean;
   onPress: () => void;
 
 }
 
-const CalculatorButton = ({label, color = Colors.darkGray, blackText = false, onPress}: Props) => {
+const CalculatorButton = ({label, color = Colors.darkGray, doubleSize = false, blackText = false, onPress}: Props) => {
   return (
-    <Pressable style={globalStyles.button} onPress={onPress}>
+    <Pressable style={({pressed}) => ({
+      ...globalStyles.button,
+      backgroundColor: color,
+      opacity: pressed ? 0.8 : 1,
+      width: doubleSize ? 180 : 80,
+    })} onPress={onPress}>
       <Text style={{
         ...globalStyles.buttonText,
         color: blackText ? 'black' : 'white'
